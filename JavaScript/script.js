e6e1fac0c5bae450fd4024b44543322a392e96ed
Aida Langholm <aidalangholm@gmail.com>
2024-09-25 13:57:43 +0200

    feat: :sparkles: start javascript
    created buttons, array of questions, score
    
    refs #4

---
+++ JavaScript/script.js
@@ -0,0 +1,57 @@
+
+const questionText =document.getElementById("question");
+const nesteKnapp = document.getElementById("next");
+const valgKnapper = document.getElementById("options");
+
+/*_________________________________________*/
+
+nesteKnapp.addEventListener("click" , event => {
+    
+   });
+/*_________Array_med_spørsmål___________________*/
+
+const questions = [{
+    spørsmål:"spørsmål 1",
+        svar:["a","b","c","d"],
+        riktigSvar:"b"
+}  ,{
+    spørsmål:"spørsmål 2",
+    svar:["e","f","g","h"],
+    riktigSvar:"h"  
+}];
+/*_________________________________________*/
+
+let spørmålIndex = 0;
+let score = 0;
+
+/*________Funksjoner____________*/
+
+function quiz(){
+ spørmålIndex = 0;
+ score = 0;
+ displayQuestion();
+}
+
+function displayQuestion() {
+    let currentQuestion = questions[spørmålIndex];
+
+    questionText.innerHTML = currentQuestion.spørsmål;
+
+    valgKnapper.innerHTML = ""; 
+
+    for (let i = 0; i < currentQuestion.svar.length; i++) {
+
+        const button = document.createElement("button");
+        button.innerHTML = currentQuestion.svar[i];
+
+        button.setAttribute("class", "valg");
+        valgKnapper.appendChild(button);
+        button.addEventListener("click",svarSkjekk)
+    }
+}
+
+function svarSkjekk(){
+    spørmålIndex++
+    console.log(spørmålIndex)
+}
+displayQuestion()
